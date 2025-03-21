a30717cf91109cba38c2f0b40d6165c3e46132fd
Thao Le <156137045+Thaole0120@users.noreply.github.com>
2025-03-21 04:35:46 +0000

    Merge main and resolve header conflict

--- app.py
+++ app.py
@@ -76,17 +76,16 @@ button {
     </style>
 </head>
 <body>
-
-<header>
-    <h1>My Favorite Recipes</h1>
-    <p>A collection of delicious recipes to try at home</p>
-    <nav>
-        <a href="{{ url_for('index') }}">Home</a>
-        <a href="{{ url_for('add_recipe') }}">Add Recipe</a>
-        <a href="#">About</a>
-        <a href="#">Contact</a>
-    </nav>
-</header>
+    <header>
+        <h1>My Favorite Recipes</h1>
+        <p>A collection of delicious recipes to try at home</p>
+        <nav>
+            <a href="{{ url_for('index') }}">Home</a>
+            <a href="{{ url_for('add_recipe') }}">Add Recipe</a>
+            <a href="#">About</a>
+            <a href="#">Contact</a>
+        </nav>
+    </header>
     
     <main>
 '''
